a91d04eee9acc54f6b93658e766a48263de7ee5f
ibrahim5638 <tolaniogunjimi49@gmail.com>
2026-01-22 15:43:45 -0500

    add reminder system python code

---
+++ reminder.py
@@ -0,0 +1,30 @@
+# Simple-reminder-system
+* Name : Ogunjimi Omotolani Ibrahim
+* Matric no : 24/15661
+* Department : Computer Science
+
+# 1. Requirement Analysis
+This system allows users to set reminders and view them later.
+
+
+
+# 2. System Design
+# Data Structure
+reminders = []
+
+# Functions
+* add_reminder() - Adds reminder
+* view_reminders() - Displays reminders
+* main() - Controls flow
+
+
+# 3. Testing
+Reminders were added and retrieved successfully.
+
+
+# 4. Deployment
+Project deployed on GitHub.
+
+
+# 5. Maintenance
+Notifications and alarms can be integrated.
